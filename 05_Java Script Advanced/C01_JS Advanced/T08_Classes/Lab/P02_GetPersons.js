07328244113e1f0e9778351dc80e90da3283db4f
Stoyan Grozdanov <74171353+StGrozdanov@users.noreply.github.com>
2021-09-30 15:32:09 +0300

    Add files via upload

---
+++ 05_Java Script Advanced/C01_JS Advanced/T08_Classes/Lab/P02_GetPersons.js
@@ -0,0 +1,31 @@
+function result(){
+    class Person {
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+    
+    let data = [];
+
+    const anna = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const softUni = new Person('SoftUni');
+    const stephan = new Person('Stephan', 'Johnson', 25);
+    const gabriel = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    data.push(anna);
+    data.push(softUni);
+    data.push(stephan);
+    data.push(gabriel);
+
+    return data;
+}
+
+let getPersons = result;
+let persons = getPersons();
+console.log(persons);
