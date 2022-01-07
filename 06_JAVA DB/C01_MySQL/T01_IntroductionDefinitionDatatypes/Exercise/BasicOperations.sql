7d7490c82a2e648dae3a76b5e0fdc26f192865f4
Stoyan Grozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-01-07 16:35:13 +0200

    Update BasicOperations.sql

--- 06_JAVA DB/C01_MySQL/T01_IntroductionDefinitionDatatypes/Exercise/BasicOperations.sql
+++ 06_JAVA DB/C01_MySQL/T01_IntroductionDefinitionDatatypes/Exercise/BasicOperations.sql
@@ -242,3 +242,82 @@ VALUES ('Ivan', 'Ivanov'),('Ivan1', 'Ivanov1'), ('Ivan2', 'Ivanov2');
 
 INSERT INTO `rental_orders`(`employee_id`,`car_id`)
 VALUES (1, 1),(1, 2), (2, 3);
+
+CREATE DATABASE `soft_uni`;
+USE `soft_uni`;
+
+CREATE TABLE `towns` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `addresses` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `address_text` TEXT NOT NULL,
+    `town_id` INT NOT NULL,
+	CONSTRAINT fk_addresses_towns
+	FOREIGN KEY (`town_id`)
+	REFERENCES `towns`(`id`)
+);
+
+CREATE TABLE `departments` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `employees` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `first_name` VARCHAR(40) NOT NULL,
+    `middle_name` VARCHAR(40) NOT NULL,
+    `last_name` VARCHAR(40) NOT NULL,
+    `job_title` VARCHAR(50) NOT NULL,
+    `department_id` INT,
+	CONSTRAINT fk_employees_departments
+	FOREIGN KEY (`department_id`)
+	REFERENCES `departments`(`id`),
+    `hire_date` DATE NOT NULL,
+    `salary` DECIMAL(8, 2) NOT NULL,
+    `address_id` INT,
+    CONSTRAINT fk_employees_adresses
+    FOREIGN KEY (`address_id`)
+    REFERENCES `addresses`(`id`)
+);
+
+INSERT INTO `towns`
+VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna'),
+(4, 'Burgas');
+
+SELECT * FROM `towns`;
+
+INSERT INTO `departments`
+VALUES
+(1, 'Engineering'),
+(2, 'Sales'),
+(3, 'Marketing'),
+(4, 'Software Development'),
+(5, 'Quality Assurance');
+
+INSERT INTO `employees`(`id`, `first_name`,`middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`, `address_id`)
+VALUES
+(1, 'Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', '3500.00', NULL),
+(2, 'Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', '4000.00', NULL),
+(3, 'Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', '525.25', NULL),
+(4, 'Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', '3000.00', NULL),
+(5, 'Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', '599.88', NULL);
+
+SELECT * FROM `towns` ORDER BY `name`;
+SELECT * FROM `departments` ORDER BY `name`;
+SELECT * FROM `employees` ORDER BY `salary` DESC;
+
+SELECT `name` FROM `towns` ORDER BY `name`;
+SELECT `name` FROM `departments` ORDER BY `name`;
+SELECT `first_name`, `last_name`, `job_title`, `salary` FROM `employees` ORDER BY `salary` DESC;
+
+UPDATE `employees`
+SET `salary` = `salary` * 1.1;
+SELECT `salary` FROM `employees`;
+
+DELETE FROM `occupancies`
